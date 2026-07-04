0ef853ce1c13a1df30e1437d2e5edced9280332b
j_a_ <jerome.arfouche@pm.me>
2026-07-04 15:34:32 -0400

    bugfix: restore results symlink on deploy

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -5,6 +5,7 @@ cd $FORGE_RELEASE_DIRECTORY
 $pnpm_path install --frozen-lockfile
 
 ln -s /mnt/volume-tor1-01/weatherfonts public/fonts
+ln -s /mnt/volume-tor1-01/bdi2-results results
 
 $pnpm_path build
 
