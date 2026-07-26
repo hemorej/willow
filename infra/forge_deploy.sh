cf18479cbc6b2b5c3090a20f7ff2f99eca169826
j_a_ <jerome.arfouche@pm.me>
2026-07-26 12:32:55 -0400

    update deploy script for new volume

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -4,7 +4,7 @@ cd $FORGE_RELEASE_DIRECTORY
 
 $PNPM_PATH install --frozen-lockfile
 
-ln -s /mnt/volume-tor1-01/willowfonts public/fonts
+ln -s /mnt/$VOLUME_NAME/willowfonts public/fonts
 
 $PNPM_PATH build
 
